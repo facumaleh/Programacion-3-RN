202ec20b4e54afc73dee4b8b0d176e6256e052d1
Facu <fmaleh@udesa.edu.ar>
2021-05-14 13:53:07 -0300

    prueba 1

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, {Component} from "react";
+import { View, SafeAreaView ,ScrollView,TextInput,} from 'react-native'
+import {  ListItem, Button, Icon, Avatar , SearchBar} from 'react-native-elements'
+
+
+
+class App extends Component {
+
+  
+    constructor(){
+        super();
+        this.state = {
+          
+            person: [],
+            search: '',
+
+        }
+    }
+
+    //solicitud api
+componentDidMount(){
+  fetch('https://randomuser.me/api/?results=10')
+  .then(response => response.json())
+  .then ((data)=>{
+    this.setState({ person: data.results});
+    console.log(this.state.person)
+
+  })
+  .catch((e)=>console.log(e));
+  }
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+
+  
+    render(){
+
+
+
+      
+        return(
+          
+          <ScrollView>
+            <SafeAreaView>
+            <View>
+              <SearchBar placeholder="Type Here..." onChangeText={this.updateSearch}value={this.state.search}/>
+        
+            { 
+              this.state.person.map((person) => (
+                <ListItem key={person.login.uuid} bottomDivider>
+                <Avatar  size="large" source={person.picture.large} />
+                <ListItem.Content>
+                  <ListItem.Title>Name:{person.name.first},{person.name.last}</ListItem.Title>
+                  <ListItem.Subtitle>Location:{person.location.city},{person.location.state}</ListItem.Subtitle>
+                  <ListItem.Subtitle>Birthdate:{person.dob.date.substring(0,10)} </ListItem.Subtitle>
+                  <ListItem.Subtitle>Current age: {person.dob.age}</ListItem.Subtitle>
+  
+                </ListItem.Content>
+                <Button
+              title="+ info" type="outline"/>
+              </ListItem>
+              ))
+            }
+            
+          </View>
+          </SafeAreaView>
+          </ScrollView>
+          
+
+        )
+    }
+    
+}
+
+export default App;
