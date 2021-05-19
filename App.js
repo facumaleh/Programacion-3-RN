7a1528b9d6cb5a011b9ee3be1e51c032f7123bb8
Agustín <Ihidoype>
2021-05-19 15:01:46 -0300

    Armado de styles aparte

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import { View, SafeAreaView ,ScrollView,TextInput, Image,Text,Pressable} from 'react-native'
 import {  ListItem,  Icon, Avatar , SearchBar, BottomSheet,Input, Header,buttonAZZA} from 'react-native-elements'
+import { styles } from './styles'
 
 
 
@@ -171,123 +172,123 @@ componentDidMount(){
     
 }
 
-const styles = {
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    with:200,
-    backgroundColor:"#ffde7d"
+// const styles = {
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     with:200,
+//     backgroundColor:"#ffde7d"
     
     
-  },
-  card: {
-    borderWidth: 3,
-    borderRadius: 10,
-    borderColor: '#00b8a9',
-    width: 300,
-    height: 450,
-    padding: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: '#f8f3d4', 
+//   },
+//   card: {
+//     borderWidth: 3,
+//     borderRadius: 10,
+//     borderColor: '#00b8a9',
+//     width: 300,
+//     height: 450,
+//     padding: 20,
+//     marginTop: 10,
+//     marginBottom: 10,
+//     backgroundColor: '#f8f3d4', 
 
-  },
-  imgCard:{
-    width: 150, 
-    height: 150, 
-    borderRadius: 10,
-   marginLeft: 50
-  },
-input:{
-  height: 40,
-  backgroundColor: 'azure', 
-  fontSize: 20,
-  borderWidth: 3,
-  borderRadius: 10,
-  borderColor: '#00b8a9',
-  marginTop: 10,
-  marginBottom: 10,
-  width:350,
+//   },
+//   imgCard:{
+//     width: 150, 
+//     height: 150, 
+//     borderRadius: 10,
+//    marginLeft: 50
+//   },
+// input:{
+//   height: 40,
+//   backgroundColor: 'azure', 
+//   fontSize: 20,
+//   borderWidth: 3,
+//   borderRadius: 10,
+//   borderColor: '#00b8a9',
+//   marginTop: 10,
+//   marginBottom: 10,
+//   width:350,
 
 
-},
-Titulo:{
- fontSize: 25,
- marginTop: 10,
- marginBottom: 10,
-},
+// },
+// Titulo:{
+//  fontSize: 25,
+//  marginTop: 10,
+//  marginBottom: 10,
+// },
 
-TextoCard:{
-  fontSize: 15,
-  marginTop: 10,
-  marginBottom: 10,
+// TextoCard:{
+//   fontSize: 15,
+//   marginTop: 10,
+//   marginBottom: 10,
  
  
- },
- SearchBar: {
-  height: 50,
-  width:350,
-  borderWidth: 1,
-  paddingLeft: 20,
-  margin: 5,
-  borderColor: '#00b8a9',
-  backgroundColor: '#FFFFFF',
-  marginTop: 10,
-  marginBottom: 10,
-  borderWidth: 3,
-  borderRadius: 10,
-  backgroundColor: 'azure', 
+//  },
+//  SearchBar: {
+//   height: 50,
+//   width:350,
+//   borderWidth: 1,
+//   paddingLeft: 20,
+//   margin: 5,
+//   borderColor: '#00b8a9',
+//   backgroundColor: '#FFFFFF',
+//   marginTop: 10,
+//   marginBottom: 10,
+//   borderWidth: 3,
+//   borderRadius: 10,
+//   backgroundColor: 'azure', 
   
-},
-button: {
-  alignItems: 'center',
-  justifyContent: 'center',
-  paddingVertical: 12,
-  paddingHorizontal: 32,
-  elevation: 3,
-  backgroundColor: 'azure', 
-  marginTop: 10,
-  borderWidth: 3,
-  borderRadius: 10,
-  borderColor: '#00b8a9',
-  with:400
-},
-buttonVerMas: {
-  alignItems: 'center',
-  justifyContent: 'center',
-  paddingVertical: 12,
-  paddingHorizontal: 32,
-  elevation: 3,
-  backgroundColor: 'azure', 
-  marginTop: 10,
-  borderWidth: 3,
-  borderRadius: 10,
-  borderColor: '#00b8a9',
-  with:400
-},
-buttonAZZA: {
-  flex: 1, 
-marginLeft:10,
-  alignItems: 'center',
-  justifyContent: 'center',
-  paddingVertical: 12,
-  paddingHorizontal: 32,
-  elevation: 3,
-  backgroundColor: 'azure', 
-  marginTop: 10,
-  borderWidth: 3,
-  borderRadius: 10,
-  borderColor: '#f6416c',
-},
-textPresables: {
-  fontSize: 16,
-  lineHeight: 21,
-  fontWeight: 'bold',
-  letterSpacing: 0.25,
-  color: 'black',
-},
-};
+// },
+// button: {
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   paddingVertical: 12,
+//   paddingHorizontal: 32,
+//   elevation: 3,
+//   backgroundColor: 'azure', 
+//   marginTop: 10,
+//   borderWidth: 3,
+//   borderRadius: 10,
+//   borderColor: '#00b8a9',
+//   with:400
+// },
+// buttonVerMas: {
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   paddingVertical: 12,
+//   paddingHorizontal: 32,
+//   elevation: 3,
+//   backgroundColor: 'azure', 
+//   marginTop: 10,
+//   borderWidth: 3,
+//   borderRadius: 10,
+//   borderColor: '#00b8a9',
+//   with:400
+// },
+// buttonAZZA: {
+//   flex: 1, 
+// marginLeft:10,
+//   alignItems: 'center',
+//   justifyContent: 'center',
+//   paddingVertical: 12,
+//   paddingHorizontal: 32,
+//   elevation: 3,
+//   backgroundColor: 'azure', 
+//   marginTop: 10,
+//   borderWidth: 3,
+//   borderRadius: 10,
+//   borderColor: '#f6416c',
+// },
+// textPresables: {
+//   fontSize: 16,
+//   lineHeight: 21,
+//   fontWeight: 'bold',
+//   letterSpacing: 0.25,
+//   color: 'black',
+// },
+// };
  
  
 
--- styles.js
+++ styles.js
@@ -1,7 +1,121 @@
 import { StyleSheet} from "react-native";
 
-const styles =StyleSheet.create( {
-  
-  
-  });
- export {styles,TextoCard,textPresables,button,SearchBar,TextoCard,Titulo,input,imgCard,card,container,buttonAZZA}  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // width:200,
+    backgroundColor:"#ffde7d"
+  },
+
+  card: {
+    borderWidth: 3,
+    borderRadius: 10,
+    borderColor: '#00b8a9',
+    width: 300,
+    height: 450,
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: '#f8f3d4', 
+  },
+
+  imgCard:{
+    width: 150, 
+    height: 150, 
+    borderRadius: 10,
+   marginLeft: 50
+  },
+
+  input:{
+  height: 40,
+  backgroundColor: 'azure', 
+  fontSize: 20,
+  borderWidth: 3,
+  borderRadius: 10,
+  borderColor: '#00b8a9',
+  marginTop: 10,
+  marginBottom: 10,
+  // width:350,
+},
+
+Titulo:{
+ fontSize: 25,
+ marginTop: 10,
+ marginBottom: 10,
+},
+
+TextoCard:{
+  fontSize: 15,
+  marginTop: 10,
+  marginBottom: 10,
+ },
+
+ SearchBar: {
+  height: 50,
+  width:350,
+  borderWidth: 1,
+  paddingLeft: 20,
+  margin: 5,
+  borderColor: '#00b8a9',
+  backgroundColor: '#FFFFFF',
+  marginTop: 10,
+  marginBottom: 10,
+  borderWidth: 3,
+  borderRadius: 10,
+  backgroundColor: 'azure', 
+},
+
+button: {
+  alignItems: 'center',
+  justifyContent: 'center',
+  paddingVertical: 12,
+  paddingHorizontal: 32,
+  elevation: 3,
+  backgroundColor: 'azure', 
+  marginTop: 10,
+  borderWidth: 3,
+  borderRadius: 10,
+  borderColor: '#00b8a9',
+  // width:400
+},
+
+buttonVerMas: {
+  alignItems: 'center',
+  justifyContent: 'center',
+  paddingVertical: 12,
+  paddingHorizontal: 32,
+  elevation: 3,
+  backgroundColor: 'azure', 
+  marginTop: 10,
+  borderWidth: 3,
+  borderRadius: 10,
+  borderColor: '#00b8a9',
+  // width:400
+},
+
+buttonAZZA: {
+  flex: 1, 
+  marginLeft:10,
+  alignItems: 'center',
+  justifyContent: 'center',
+  paddingVertical: 12,
+  paddingHorizontal: 32,
+  elevation: 3,
+  backgroundColor: 'azure', 
+  marginTop: 10,
+  borderWidth: 3,
+  borderRadius: 10,
+  borderColor: '#f6416c',
+},
+
+textPresables: {
+  fontSize: 16,
+  lineHeight: 21,
+  fontWeight: 'bold',
+  letterSpacing: 0.25,
+  color: 'black',
+  }
+});
+ export {styles}  
